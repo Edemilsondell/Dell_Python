96eb95f5387fc16be82e55d775fcc19df885e058
Edemilson Da Mata <edemilsondell@hotmail.com>
2023-07-04 15:58:44 -0400

    atualização 2 baskara

--- baskara.py
+++ baskara.py
@@ -17,6 +17,7 @@ ax = float(a)
 bx = float(b)
 cx = float(c)
 
+
 def calcular_baskara(a, b, c):
     discriminante = b**2 - 4*a*c
     print("\nDelta = ", discriminante)
@@ -31,9 +32,7 @@ def calcular_baskara(a, b, c):
     else:
         return "Impossivel calcular."
     
-    
-
-
+   
 v1 = input("Digite os valores para A:")
 v2 = input("Digite os valores para B:")
 v3 = input("Digite os valores para C:")
@@ -41,16 +40,28 @@ v3 = input("Digite os valores para C:")
 os.system("cls") # Limpa o terminal
 print("Primeira formula")
 raizes = calcular_baskara(ax, bx, cx)
-print(raizes)
+rList = list(raizes)
+print(raizes) # imprimi em forma de Tupla
+print(rList)  # imprimi em forma de lista
+print(rList[0]) # imprimi por indice da lista
+print(rList[1]) # imprimi por indice da lista
 
+x1 = float((rList[0]))
+x2 = float((rList[1]))
+print('R1 = {:.3f}'.format(x1)) 
+print('R1 = {:.3f}'.format(x2)) 
+# print(type(raizes))
+# print(type(rList))
 
 print()
+
+
 
 a = float(v1)
 b = float(v2)
 c = float(v3)
 
-deltax =b**2 - 4*a*c
+deltax = b**2 - 4*a*c
 print("Segunda formula")
 print()
 print("Delta", deltax)
